287c7930eaf5f89757558032492f5e2408591823
kalljuha <kallioniemi.juha@hotmail.com>
2019-10-15 00:44:25 +0300

    some localization done

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import * as React from "react";
 import { Header } from "./components/Header";
-import { Home } from "./components/Home";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import "./i18n";
 
 const App: React.FC = () => {
+    const { t, i18n } = useTranslation();
+    // const changeLanguage = (lang: any) => {
+    //     i18n.changeLanguage(lang);
+    // };
+
     return (
-        <React.Fragment>
+        <Router>
             <header>
-                <Header />
+                <Route
+                    component={(props: any) => (
+                        <Header t={t} i18n={i18n} {...props} />
+                    )}
+                />
             </header>
-            <Home compiler="TypeScript" framework="React" />
             <footer>Created by Juha Kallioniemi</footer>
-        </React.Fragment>
+        </Router>
     );
 };
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,65 @@
 import * as React from "react";
+import { History, LocationState } from "history";
+import { TFunction, i18n } from "i18next";
 
-// 'HelloProps' describes the shape of props.
-// State is never set so we use the '{}' type.
-export class Header extends React.Component {
-    // menuClick = () => {
-    //     this.props.history.push("/AboutMe");
-    // };
+interface HeaderProps {
+    history?: History<LocationState>;
+    t: TFunction;
+    i18n: i18n;
+    isEnglish: boolean;
+}
+
+export class Header extends React.Component<HeaderProps, {}> {
+    changeLanguage = (lang: string) => {
+        this.props.i18n.changeLanguage(lang);
+    };
+
+    isLangEnglish: boolean = this.props.i18n.language == "en" ? true : false;
+
+    renderLocaleButton(): JSX.Element {
+        if (!this.isLangEnglish) {
+            return (
+                <i
+                    className="flag-en"
+                    onClick={() => this.changeLanguage("en")}
+                ></i>
+            );
+        } else {
+            return (
+                <i
+                    className="flag-fi"
+                    onClick={() => this.changeLanguage("fi")}
+                ></i>
+            );
+        }
+    }
+
+    menuButtons: string[] = ["AboutMe", "Projects", "Contact"];
+
+    menuClick = (linkName: string) => {
+        this.props.history.push("#" + linkName);
+    };
 
     render() {
         return (
             <React.Fragment>
-                <h1 className="portfolio-title">Portfolio</h1>
+                <div className="portfolio-title">
+                    <h1 onClick={() => this.props.history.push("/")}>
+                        Portfolio
+                    </h1>
+                </div>
+                <div className="flag-icon">{this.renderLocaleButton()}</div>
                 <div className="menu-links">
-                    {/* <a href="#" onClick={this.menuClick}>
-                        About me
-                    </a> */}
-                    <a>Projects</a>
-                    <a>Contact</a>
+                    {this.menuButtons.map(btn => (
+                        <button
+                            key={btn}
+                            type="button"
+                            className="btn btn-info link"
+                            onClick={() => this.menuClick(btn)}
+                        >
+                            {btn}
+                        </button>
+                    ))}
                 </div>
             </React.Fragment>
         );
