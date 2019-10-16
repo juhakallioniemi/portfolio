c24d96a0621b059f0004b5abaf7ef10fe43cc9f4
kalljuha <kallioniemi.juha@hotmail.com>
2019-10-16 16:16:07 +0300

    test

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,22 +9,12 @@ interface FooterProps {
 }
 
 export class Footer extends React.Component<FooterProps, {}> {
-    fileWithDate = new File([], "/docs/main.js", {
-        lastModified: new Date(2017, 1, 1) as any
-    });
-
-    lastUpdate = () => {
-        return new Date(this.fileWithDate.lastModified).toLocaleDateString(
-            this.props.i18n.language
-        );
-    };
-
     render() {
         console.log(document.lastModified);
         return (
             <React.Fragment>
                 <div>
-                    {this.props.t("lastUpdate")}: {this.lastUpdate()}
+                    {this.props.t("lastUpdate")}: {document.lastModified}
                 </div>
             </React.Fragment>
         );
