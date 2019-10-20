c5622d24227105a1b0b1ecc3313013d16d10efc1
kalljuha <kallioniemi.juha@hotmail.com>
2019-10-20 13:57:07 +0300

    test

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,10 @@ import * as ReactDOM from "react-dom";
 import "../src/styles/main.less";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+const browserslist = require("browserslist");
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
+
+browserslist.clearCaches();
