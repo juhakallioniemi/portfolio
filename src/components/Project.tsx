c8395ca7fd498ce01b208da167e21d34c7f7bec5
kalljuha <kallioniemi.juha@hotmail.com>
2019-10-21 18:58:49 +0300

    project border animations and project running initialized

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { TFunction, i18n } from "i18next";
 
 const appEnvironment = process.env.NODE_ENV;
 const brandGameUrl =
@@ -8,22 +9,39 @@ const brandGameUrl =
 
 interface ProjectProps {
     projectName: string;
+    clickedProject: any;
+    t: TFunction;
+    i18n: i18n;
 }
 
-export class Project extends React.Component<ProjectProps, {}> {
+interface ProjectState {
+    projectInfo: string;
+}
+
+export class Project extends React.Component<ProjectProps, ProjectState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            projectInfo: this.props.projectName
+        };
+    }
+    openProject = () => {
+        if (this.props.projectName === this.props.t("brand-game")) {
+            this.props.clickedProject(brandGameUrl);
+        } else {
+            this.setState({
+                projectInfo: this.props.t("noContent")
+            });
+        }
+    };
+
     render() {
         return (
             <React.Fragment>
-                <div className="project">{this.props.projectName}</div>
+                <div className="project" onClick={this.openProject}>
+                    {this.state.projectInfo}
+                </div>
             </React.Fragment>
-            // <React.Fragment>
-            //     <object
-            //         type="text/html"
-            //         data={brandGameUrl}
-            //         width="800px"
-            //         height="600px"
-            //     ></object>
-            // </React.Fragment>
         );
     }
 }
--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -7,14 +7,73 @@ interface ProjectsListProps {
     i18n: i18n;
 }
 
-export class ProjectsList extends React.Component<ProjectsListProps, {}> {
+interface ProjectsListState {
+    isProjectRunning: boolean;
+    projectUrl: string;
+}
+
+export class ProjectsList extends React.Component<
+    ProjectsListProps,
+    ProjectsListState
+> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            isProjectRunning: false,
+            projectUrl: ""
+        };
+    }
+
+    openClickedProject = (projectUrl: string) => {
+        this.setState({
+            projectUrl: projectUrl,
+            isProjectRunning: true
+        });
+    };
+
     render() {
-        return (
-            <div className="projects-list">
-                <Project projectName={this.props.t("brand-game")} />
-                <Project projectName="TODO" />
-                <Project projectName="TODO" />
-            </div>
-        );
+        // TODO: object width and height 100% somehow
+        // try .content styling from less
+        // Save running project state somehow
+        var testHeight = "500px";
+
+        if (this.state.isProjectRunning) {
+            return (
+                <div className="projects-list running-project">
+                    <React.Fragment>
+                        <object
+                            className="content"
+                            type="text/html"
+                            data={this.state.projectUrl}
+                            width="800px"
+                            height={testHeight}
+                        ></object>
+                    </React.Fragment>
+                </div>
+            );
+        } else {
+            return (
+                <div className="projects-list">
+                    <Project
+                        projectName={this.props.t("brand-game")}
+                        clickedProject={this.openClickedProject}
+                        t={this.props.t}
+                        i18n={this.props.i18n}
+                    />
+                    <Project
+                        projectName="TODO"
+                        clickedProject={this.openClickedProject}
+                        t={this.props.t}
+                        i18n={this.props.i18n}
+                    />
+                    <Project
+                        projectName="TODO"
+                        clickedProject={this.openClickedProject}
+                        t={this.props.t}
+                        i18n={this.props.i18n}
+                    />
+                </div>
+            );
+        }
     }
 }
