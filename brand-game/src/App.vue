827603796d1bed111382f050c6a1aaf74dfc0c7e
kalljuha <kallioniemi.juha@hotmail.com>
2020-10-24 11:51:17 +0300

    asd

--- brand-game/src/App.vue
+++ brand-game/src/App.vue
@@ -1,11 +1,11 @@
 <template>
-	<div id="brand-game">
-		<h1 style="margin: 0">{{ gameTitle }}</h1>
-		<div>{{ message }}</div>
-		<!-- <div>Counter: {{ counter }}</div>
+    <div id="brand-game">
+        <h1 style="margin: 0">{{ gameTitle }}</h1>
+        <div>{{ message }}</div>
+        <!-- <div>Counter: {{ counter }}</div>
 		<button v-on:click="increment">Increment</button>
 		<Hello />-->
-	</div>
+    </div>
 </template>
 
 <script>
@@ -14,28 +14,29 @@ import Component from "vue-class-component";
 import Hello from "./components/Hello.vue";
 
 @Component({
-	components: {
-		Hello,
-	},
+    components: {
+        Hello
+    }
 })
 export default class App extends Vue {
-	gameTitle = "Brand game";
-	message = "Work in progress...";
-	counter = 0;
-	increment() {
-		this.counter++;
-	}
+    gameTitle = "Brand game";
+    message = "Work in progress...";
+    counter = 0;
+    increment() {
+        this.counter++;
+    }
 
-	created() {
-		var arr = [8, 2, 4, 5, 2, 8];
-		var sorted = arr.slice().sort(function (a, b) {
-			return a - b;
-		});
-		console.log(sorted);
-		var ranks = sorted.map(function (v, index) {
-			return sorted.indexOf(v) + 1;
-		});
-		console.log(ranks);
-	}
+    created() {
+        var arr = [8, 2, 4, 5, 2, 8];
+        var sorted = arr.slice().sort(function(a, b) {
+            return a - b;
+        });
+        console.log(sorted);
+        var ranks = sorted.map(function(v, index) {
+            console.log(index);
+            return sorted.indexOf(v) + 1;
+        });
+        console.log(ranks);
+    }
 }
 </script>
